9142a7001e992a1a7279b02388c5f9f5e0473585
Cristian Karmy Guzman <>
2019-02-25 15:18:21 -0300

    Save custom panel example

--- MainView.js
+++ MainView.js
@@ -1,19 +1,42 @@
+var Share = require("FuseJS/Share")
 var SignatureModule = require("SignatureModule");
 
-function save() {
-	sp.save("test.png");
-}
+//Signature
+	function save() {
+		sp.save("signature.png");
 
-function clear() {
-	Mysign.clear();
-}
+		setTimeout(function() {
+			getPath();
+		}, 500);
+	}
 
-function getPath() {
-	console.log("Path from JS: " + SignatureModule.GetPath('sp'));
-}
+	function clear() {
+		Mysign.clear();
+	}
+
+	function getPath() {
+		console.log("Path from JS: " + SignatureModule.GetPath('sp'));
+		Share.shareFile(SignatureModule.GetPath('sp'), "image/*", "SavePanel1 from Fuse");
+	}
+
+//Save custom Panel
+	function saveCustomPanel1() {
+		//Save panel
+		SavePanel1.save("SavePanel1.png");
+
+		setTimeout(function() {
+			shareCustomPanel();
+		}, 500);
+	}
+
+	function shareCustomPanel() {
+		console.log("Path from JS: " + SignatureModule.GetPath('SavePanel1'));
+		Share.shareFile(SignatureModule.GetPath('SavePanel1'), "image/*", "SavePanel1 from Fuse");
+	}
 
 module.exports = {
 	save: save,
 	clear: clear,
-	getPath: getPath
+	getPath: getPath,
+	saveCustomPanel1: saveCustomPanel1
 }
