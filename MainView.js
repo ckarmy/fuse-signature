6b8884b35b5ceab9750c882231b4a26932e76205
Cristian Karmy Guzman <>
2019-02-25 15:50:26 -0300

    Fixs

--- MainView.js
+++ MainView.js
@@ -19,24 +19,40 @@ var SignatureModule = require("SignatureModule");
 		Share.shareFile(SignatureModule.GetPath('sp'), "image/*", "SavePanel1 from Fuse");
 	}
 
-//Save custom Panel
+//Save custom Panel 1
 	function saveCustomPanel1() {
 		//Save panel
 		SavePanel1.save("SavePanel1.png");
 
 		setTimeout(function() {
-			shareCustomPanel();
+			shareCustomPanel1();
 		}, 500);
 	}
 
-	function shareCustomPanel() {
+	function shareCustomPanel1() {
 		console.log("Path from JS: " + SignatureModule.GetPath('SavePanel1'));
 		Share.shareFile(SignatureModule.GetPath('SavePanel1'), "image/*", "SavePanel1 from Fuse");
+	}
+
+//Save custom Panel 2
+	function saveCustomPanel2() {
+		//Save panel
+		SavePanel2.save("SavePanel2.png");
+
+		setTimeout(function() {
+			shareCustomPanel2();
+		}, 500);
+	}
+
+	function shareCustomPanel2() {
+		console.log("Path from JS: " + SignatureModule.GetPath('SavePanel2'));
+		Share.shareFile(SignatureModule.GetPath('SavePanel2'), "image/*", "SavePanel2 from Fuse");
 	}
 
 module.exports = {
 	save: save,
 	clear: clear,
 	getPath: getPath,
-	saveCustomPanel1: saveCustomPanel1
+	saveCustomPanel1: saveCustomPanel1,
+	saveCustomPanel2: saveCustomPanel2
 }
---
+++ README.md
@@ -0,0 +1,8 @@
+## This project is forked from bolav signature/save module
+
+This app contains:
+- A example to draw something and share it.
+- A example to share any Panel or section of the app.
+
+To work you need to use this PR of the fuselibs (https://github.com/fuse-open/fuselibs/pull/1253). 
+For more information visit https://medium.com/@andrewq/hello-world-eaa996c594b7
